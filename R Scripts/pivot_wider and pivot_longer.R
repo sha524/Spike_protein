3dfc70b9c3536867a5d843a6a5f3703294cd7837
Sam Aldous <sha524@york.ac.uk>
2025-01-26 17:12:21 +0000

    Creating the data frame to work from

--- R Scripts/pivot_wider and pivot_longer.R
+++ R Scripts/pivot_wider and pivot_longer.R
@@ -3,3 +3,23 @@
 
 
 ###### What does this R script contain ######
+
+#Attemping to get the data in pivot_wider format, wide data
+
+
+
+
+###### Data Manipulation ######
+
+##Going to select just the Sequence Information and Mutations columns
+##Will just be easier to work with
+
+view(practice_spike_df)
+sequence_mutations_data <- practice_spike_df %>%
+  select(Sequence_Information, Mutations) %>%
+view()
+
+sequence_mutations_data %>%
+  
+
+
