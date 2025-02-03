00ff47af956dd63e7eb4628b23c754af0523374d
Sam Aldous <sha524@york.ac.uk>
2025-02-03 17:13:37 +0000

    Plot for top 20 sequences with the most mutations

--- R Scripts/Scripts for actual data/Number of mutations per sequence.R
+++ R Scripts/Scripts for actual data/Number of mutations per sequence.R
@@ -41,8 +41,32 @@ most_number_of_mutations <- UK_sequences_df %>%
   arrange(desc(Number_of_mutations)) %>%
   head(20)
 
-ggplot(UK_sequences_df, aes(x = Sequence_Information, y = Number_of_mutations)) +
-  geom_point()
+#Plot of the sequences with the most mutations
+ggplot(most_number_of_mutations, aes(x = Sequence_Information, y = Number_of_mutations,
+                                     fill = Sequence_Information)) +
+  geom_col() +
+  ylim(0, 300) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5, size = 7)) +
+  theme(panel.background = element_rect(fill = "white")) +
+  theme(legend.position = "none") +
+  theme(axis.line = element_line(colour = "black")) +
+  ylab("Number of mutations per sequence") +
+  xlab("Sequence Information")
+
+
+
+#Plot top 10 sequences with the most mutations, non vs syn
+top_10_most_mutations <- most_number_of_mutations %>%
+  head(10)
+
+ggplot(top_10_most_mutations, aes(x = non_mutations, syn_mutations,))
+
+  
+  
+
+
+
+
 
 
 
