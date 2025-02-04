10479bccb73bb234283c24e0b55311926041d456
Sam Aldous <sha524@york.ac.uk>
2025-02-04 23:09:51 +0000

    Added a text file containing the names of important data structures

--- R Scripts/Scripts for actual data/pivot_wider.R
+++ R Scripts/Scripts for actual data/pivot_wider.R
@@ -22,6 +22,10 @@ Sequence_Mutation_columns <- UK_sequences_df %>%
   select(Sequence_Information, Mutations)
 
 
+
+
+###### Data manipulation #####
+
 #Going to use separate_longer_delim()
 #Function splits the mutations based on the delimiter |
 #and makes a new row for each split
@@ -41,9 +45,46 @@ combined_data <- Mutations_split %>%
   semi_join(most_common_mutations_UK, by = "Mutations")
   
 
+###### pivot_wider() #####
+
 #Using pivot_wider() to transform the data from long format to wide format
 wide_data_UK <- combined_data %>%
   pivot_wider(names_from = "Mutations", values_from = "Mutations")
+
+
+#Need to change all the NA's to 0's
+#Need to change all the values where the mutation is present to 1's
+no_na_UK <- wide_data_UK %>%
+  select(-Sequence_Information) %>%
+  mutate(across(everything(), ~ ifelse(is.na(.), "0", "1")))
+
+#Going to recombine with the Sequence_Information column
+#Selecting the sequence information column to then recombine with the mutation data
+sequence_information_column_2 <- wide_data_UK %>%
+  select(Sequence_Information)
+
+#completed_wide_data_UK contains the sequence information
+#and the most common mutations
+#vectorised the data. 1's represent where the mutation is present
+#0's represent where the data is not present
+completed_wide_data_UK <- data.frame(c(sequence_information_column_2, no_na_UK))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
