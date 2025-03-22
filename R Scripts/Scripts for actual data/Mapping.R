8a8817eb9cd748fdf2f18d9f61aa3d14a7ed83cc
Sam Aldous <sha524@york.ac.uk>
2025-03-22 10:49:09 +0000

    Creating the map to show the cluster distribution in the UK

---
+++ R Scripts/Scripts for actual data/Mapping.R
@@ -0,0 +1,9 @@
+################ Research Project #################
+
+#Going to use the actual data
+#UK_seqs_msa_0522_spike_mutations.tx
+
+
+##### What does this R script contain #####
+
+#Creating the map to show to the cluster distribution in the UK
